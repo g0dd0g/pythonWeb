5a646132144cabfea69b834f527d66812e1f199b
g0dd0g <tlsghwns56@gmail.com>
2020-08-07 06:40:15 +0900

    master

--- test.py
+++ test.py
@@ -13,11 +13,12 @@ ws = wb.active
 ws.title = 'Virtual-Machine'
 
 # Webdriver 사용하여 접속
-driver = webdriver.Chrome('C:\python\chromedriver')
+#driver = webdriver.Chrome('C:\python\chromedriver')
+driver = webdriver.Chrome('C:\still\pythonWeb\chromedriver')
 driver.get("https://azure.microsoft.com/ko-kr/pricing/calculator")
 
 # 페이지가 로드되는 시간을 위하여 정지
-time.sleep(7)
+time.sleep(5)
 
 # Virtual-Machine 버튼 클릭하여 추가
 vm = driver.find_element_by_xpath('//*[@id="products-picker-panel"]/div[2]/div[2]/div[1]/div/div/div[1]/button')
@@ -30,7 +31,8 @@ time.sleep(5)
 #region = driver.find_elements_by_css_selector('select[name=region]>option')
 
 currency = ['KRW', 'USD']
-cur_text = ['한국 원(₩)', '미국 달러($)']
+cur_text = ['KRW(₩)', 'USD($)']
+cur_text2 = ['₩', '$']
 cur_size = len(currency) 
 region = ['korea-central', 'korea-south']
 reg_text = ['한국 중부', '한국 남부']
@@ -46,62 +48,59 @@ sel_tier = Select(driver.find_element_by_xpath('//*[@name="tier"]'))
 
 
 col_text = ["통화", "지역", "운영 체제", "유형", "계층", "인스턴스", "인스턴스2", "인스턴스3"]
-index = 1
+col = "ABCDEFGHIJKLMNOPQRSTU"
+row_index = 4
+col_size = len(col_text)
 
 # 머리글 추가
-ws['A1'] = col_text[0]
-ws['B1'] = col_text[1]
-ws['C1'] = col_text[2]
-ws['D1'] = col_text[3]
-ws['E1'] = col_text[4]
-ws['F1'] = col_text[5]
-
-ws['G1'] = col_text[6]
-ws['H1'] = col_text[7]
+for i in range(col_size) :
+    ws[f'{col[i]}3'] = col_text[i]
 
 
 hours = driver.find_element_by_name("hours")
 hours.clear()
 hours.send_keys("1")
 
+
 sel_currency.select_by_value(currency[0])
 sel_region.select_by_value(region[0])
 sel_os.select_by_value('windows')
-sel_type.select_by_value('os-only')
+sel_type.select_by_value('sql')
 sel_tier.select_by_value('standard')
 
 sel_instance = Select(driver.find_element_by_xpath('//*[@name="size"]'))
-sel_instance.select_by_value('d8dv4')
+sel_instance.select_by_value('ds4v2')
 
 
 savings = driver.find_elements_by_class_name('savings-option')
 
-print(savings[0].find_element_by_class_name('text-heading5').text)
-"""
-for s in savings :
-    print(s.find_element_by_class_name('text-heading5').text)
-"""
+print(savings[1].find_element_by_class_name('text-heading5').text[:2])
+print(savings[1].find_element_by_css_selector('div[class=total]>span').text)
 
-index += 1
-ws[f'A{index}'] = currency[0]
-ws[f'B{index}'] = region[0]
-ws[f'C{index}'] = 'windows'
-ws[f'D{index}'] = 'os-only'
-ws[f'E{index}'] = 'standard'
+for i in range(len(savings)) :
+    radio_com = savings[i].find_elements_by_css_selector('input')
+    for rc in radio_com :
+        if rc.is_enabled() :
+            rc.click()
+            text = driver.find_element_by_css_selector('select[name=size]>option[value=ds4v2]').text
+            info = text.split(',')
+            print(info[0].split(':')[1].strip())
+            print(info[1].strip().split(' ')[0])
+            print(info[2].strip().split(' ')[0])
+            print(text[text.find(cur_text2[0]):text.find('(')])
+            print(f'{col[0]}{row_index}')
+            
+            ws[f'{col[0]}{row_index}'] = text
+            ws[f'{col[1]}{row_index}'] = info[0].split(':')[1].strip()
+            ws[f'{col[2]}{row_index}'] = info[1].strip().split(' ')[0]
+            ws[f'{col[3]}{row_index}'] = info[2].strip().split(' ')[0]
+            ws[f'{col[4]}{row_index}'] = text[text.find(cur_text2[0]):text.find('(')]
+        else :
+            for i in range(5) :
+                ws[f'F{row_index}'] = "X"
+        row_index += 1          
 
-radio_com = savings[0].find_elements_by_css_selector('input')
-
-for rc in radio_com :
-    if rc.is_enabled() :
-        rc.click()
-        ws[f'F{index}'] = driver.find_element_by_css_selector('select[name=size]>option[value=d8dv4]').text
-    else :
-        ws[f'F{index}'] = "X"
-    index += 1          
 
 wb.save("test.xlsx")
-
-
-
 
 
